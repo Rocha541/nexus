b5c964d0f4014738847444ab111c8f311341586d
Rocha541 <arthuraugustosantosrocha@gmail.com>
2025-10-21 11:50:36 -0300

    feat(users): valida e-mail único, hasheia senha e cria categorias padrão

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { PrismaService } from './database/prisma.service';
+import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-    controllers: [UsersController],
-    providers: [UsersService, PrismaService],
+    controllers: [],
+    providers: [],
+    imports: [UsersModule, DatabaseModule],
 })
 export class AppModule {}
