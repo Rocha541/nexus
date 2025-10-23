406eed3f74f6a90dee70d6fc89ca65c8a60fe24c
Rocha541 <arthuraugustosantosrocha@gmail.com>
2025-10-23 01:36:03 -0300

    feat(auth): implement JWT authentication with secure environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 
+import { AuthModule } from './modules/auth/auth.module';
+
 @Module({
     controllers: [],
     providers: [],
-    imports: [UsersModule, DatabaseModule],
+    imports: [UsersModule, DatabaseModule, AuthModule],
 })
 export class AppModule {}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { AuthenticateDto } from './dtos/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly usersRepo: UsersRepository,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async authenticate(authenticateDto: AuthenticateDto) {
+        const { email, password } = authenticateDto;
+        const user = await this.usersRepo.findUnique({
+            where: { email },
+        });
+        if (!user) {
+            throw new UnauthorizedException('Ivalid credentials');
+        }
+        const isPasswordValid = await compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Ivalid credentials');
+        }
+
+        const AcesseToken = await this.jwtService.signAsync({ sub: user.id });
+        return { AcesseToken };
+    }
+}
