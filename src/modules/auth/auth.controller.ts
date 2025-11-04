34f04c6b4d5b672697e15ae56c7a6620fa8c3786
Rocha541 <arthuraugustosantosrocha@gmail.com>
2025-11-04 10:56:42 -0300

    docs:creating initial route documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticateDto } from './dtos/authenticate.dto';
-
+import {
+    ApiOkResponse,
+    ApiOperation,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+    @ApiOperation({ summary: 'Authenticate user' })
+    @ApiOkResponse({ description: 'JWT token returned successfully.' })
+    @ApiResponse({
+        status: 200,
+        description: 'Login successful. JWT token returned..',
+    })
+    @ApiResponse({ status: 401, description: 'Invalid credentials' })
     @Post('signin')
     autenticate(@Body() authenticateDto: AuthenticateDto) {
         return this.authService.authenticate(authenticateDto);
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
-
+    @ApiOperation({ summary: 'Create user' })
+    @ApiResponse({ status: 201, description: 'User created' })
+    @ApiBody({ type: CreateUserDto })
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
